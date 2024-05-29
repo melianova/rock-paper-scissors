d5dcda800cef663b3d251740c60e4406753f49e8
melianova <170462112+melianova@users.noreply.github.com>
2024-05-28 22:02:17 -0300

    Add getHumanChoice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -7,3 +7,9 @@ function getComputerChoice() {
     const random = Math.floor(Math.random() * choices.length);
     return choices[random];
 }
+
+function getHumanChoice() {
+    const choice = prompt('Type 0 for rock, 1 for paper or 2 for scissors. Default: rock.');
+    const choices = ['rock', 'paper', 'scissors'];
+    return choices[choice];
+}
