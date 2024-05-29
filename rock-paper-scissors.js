ffa909c6fcfd0d2fd95edc3e922c192be675c0b3
melianova <170462112+melianova@users.noreply.github.com>
2024-05-29 12:57:31 -0300

    Add playRound function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -16,3 +16,38 @@ function getHumanChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     return choices[choice];
 }
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === computerChoice) {
+        return "It's a draw!";
+    }
+    if (humanChoice === 'rock')
+        if (computerChoice === 'paper') {
+            ++computerScore;
+            return 'You loose! Paper beats rock!';
+        }
+        if (computerChoice === 'scissors') {
+            ++humanScore;
+            return 'You win! Rock beats scissors!';
+        }
+    if (humanChoice === 'paper')
+        if (computerChoice === 'scissors') {
+            ++computerScore;
+            return 'You loose! Scissors beat paper!';
+        }
+        if (computerChoice === 'rock') {
+            ++humanScore;
+            return 'You win! Paper beats rock!';
+        }
+    if (humanChoice === 'scissors')
+        if (computerChoice === 'rock') {
+            ++computerScore;
+            return 'You loose! Rock beats scissors!';
+        }
+        if (computerChoice === 'paper') {
+            ++humanScore;
+            return 'You win! Scissors beat paper!';
+        }
+}
