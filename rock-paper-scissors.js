33fc8e2b847a9756ffe93ffcef8bea9442f6b488
melianova <170462112+melianova@users.noreply.github.com>
2024-05-28 21:52:28 -0300

    Add getComputerChoice function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,9 @@
 'use strict';
 
 console.log('Hello');
+
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const random = Math.floor(Math.random() * choices.length);
+    return choices[random];
+}
