de95ca03089edee8a5748cefe9a045d89abb3207
melianova <170462112+melianova@users.noreply.github.com>
2024-05-29 13:02:02 -0300

    Add playGame function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -17,37 +17,46 @@ function getHumanChoice() {
     return choices[choice];
 }
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
 
-    if (humanChoice === computerChoice) {
-        return "It's a draw!";
+
+function playGame() {
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase();
+    
+        if (humanChoice === computerChoice) {
+            return "It's a draw!";
+        }
+        if (humanChoice === 'rock')
+            if (computerChoice === 'paper') {
+                ++computerScore;
+                return 'You loose! Paper beats rock!';
+            }
+            if (computerChoice === 'scissors') {
+                ++humanScore;
+                return 'You win! Rock beats scissors!';
+            }
+        if (humanChoice === 'paper')
+            if (computerChoice === 'scissors') {
+                ++computerScore;
+                return 'You loose! Scissors beat paper!';
+            }
+            if (computerChoice === 'rock') {
+                ++humanScore;
+                return 'You win! Paper beats rock!';
+            }
+        if (humanChoice === 'scissors')
+            if (computerChoice === 'rock') {
+                ++computerScore;
+                return 'You loose! Rock beats scissors!';
+            }
+            if (computerChoice === 'paper') {
+                ++humanScore;
+                return 'You win! Scissors beat paper!';
+            }
     }
-    if (humanChoice === 'rock')
-        if (computerChoice === 'paper') {
-            ++computerScore;
-            return 'You loose! Paper beats rock!';
-        }
-        if (computerChoice === 'scissors') {
-            ++humanScore;
-            return 'You win! Rock beats scissors!';
-        }
-    if (humanChoice === 'paper')
-        if (computerChoice === 'scissors') {
-            ++computerScore;
-            return 'You loose! Scissors beat paper!';
-        }
-        if (computerChoice === 'rock') {
-            ++humanScore;
-            return 'You win! Paper beats rock!';
-        }
-    if (humanChoice === 'scissors')
-        if (computerChoice === 'rock') {
-            ++computerScore;
-            return 'You loose! Rock beats scissors!';
-        }
-        if (computerChoice === 'paper') {
-            ++humanScore;
-            return 'You win! Scissors beat paper!';
-        }
+
+    for (let i = 0; i < 5; ++i) {
+        const result = playRound(getHumanChoice(), getComputerChoice());
+        console.log(result);
+    }
 }
